e0e93486acf3609fbbe05f7c0dcbe7da83a5bf15
Dmytro.Sch <dmytro.sch@list.ru>
2020-10-17 22:38:54 +0300

    add search dom

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -3,13 +3,12 @@ import React from "react";
 import MoviesList from "../MoviesList";
 import fetchMovies from "../../utils/fetchMovies";
 
-import Error from '../Error'
+import Error from "../Error";
 
 export default class HomePage extends React.Component {
     state = {
         movies: [],
         error: false,
-        searchQuery: null,
     };
     async getPopularMovies() {
         try {
@@ -24,41 +23,18 @@ export default class HomePage extends React.Component {
     componentDidMount() {
         this.getPopularMovies();
     }
-    componentDidUpdate(prevProps, prevState) {
-        if(prevState.searchQuery !== this.state.searchQuery){
-            // this.props.history.push.
-        }
-    }
-    
-
-    searchHandler = (event) => {
-        const query = event.target.elements.searchQuery.value;
-        this.setState({
-            searchQuery: query,
-        });
-    };
 
     render() {
         const { movies, error } = this.state;
         return (
             <section>
-                {!error && (
-                    <form onSubmit={this.searchHandler}>
-                        <input
-                            type="text"
-                            name="searchQuery"
-                            placeholder="Enter your query"
-                        />
-                        <button type="submit">Find</button>
-                    </form>
-                )}
-                {movies && (
+                {movies && !error && (
                     <>
                         <h2>Trending today</h2>
                         <MoviesList moviesList={movies} />
                     </>
                 )}
-                {error && <Error/>}
+                {error && <Error />}
             </section>
         );
     }
--- src/components/MoviesPage/index.js
+++ src/components/MoviesPage/index.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import React from "react";
 
-export default function MoviesPage(){
-    return(
-        <ul>
-            <li></li>
-        </ul>
-    )
+import MoviesList from "../MoviesList";
+import Error from "../Error";
+
+import fetchMovies from "../../utils/fetchMovies";
+
+export default class MoviesPage extends React.Component {
+    state = {
+        searchQuery: null,
+        error: false,
+        films: [],
+        loading: false,
+    };
+    async componentDidUpdate(prevProps, prevState) {
+        console.log(prevState.searchQuery, this.state.searchQuery);
+        if (prevState.searchQuery !== this.state.searchQuery) {
+            this.toggleLoader();
+            try {
+                const searchResult = await fetchMovies.getFilmsBySearchQuery(
+                    this.state.searchQuery
+                );
+                this.setState({ films: searchResult });
+            } catch {
+                this.setState({ error: true });
+            } finally {
+                this.toggleLoader();
+            }
+        }
+    }
+    toggleLoader() {
+        this.setState((prevState) => ({
+            loading: !prevState.loading,
+        }));
+    }
+
+    searchHandler = (event) => {
+        event.preventDefault();
+        const query = event.target.elements.searchQuery.value;
+        if (query) {
+            this.setState({
+                searchQuery: query,
+            });
+        }
+    };
+    render() {
+        const { films, searchQuery, error, loading } = this.state;
+        return (
+            <>
+                <form onSubmit={this.searchHandler}>
+                    <input
+                        type="text"
+                        name="searchQuery"
+                        placeholder="Enter your query"
+                    />
+                    <button type="submit">Find</button>
+                </form>
+                {films.length === 0 && searchQuery && !loading && (
+                    <p>
+                        Sorry. We couldn't find any film by your query: "
+                        {searchQuery}"
+                    </p>
+                )}
+                {films.length > 0 && <MoviesList moviesList={films} />}
+                {error && <Error />}
+            </>
+        );
+    }
 }
